161b8bdce0fea429fd550e286d128985c06192f8
Hitendra <hitendrakrajat@gmail.com>
2026-03-17 23:08:52 +0530

    Updated info on tab4

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -41,7 +41,7 @@ const Index = () => (
               </Button>
             </Link>
             <Link to="/contact">
-              <Button size="lg" variant="outline" className="border-primary-foreground/30 text-primary-foreground hover:bg-primary-foreground/10">
+              <Button size="lg" variant="outline" className="border-white text-white hover:bg-white/20 bg-transparent">
                 Contact Us
               </Button>
             </Link>
